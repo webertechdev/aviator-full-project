953f927bf34ffc9df9234cb966ff795d4704adb6
WeberTechDevs <webertechdevs@gmail.com>
2026-06-11 13:48:59 +0300

    Fix import statement in deposit.js

--- api/deposit.js
+++ api/deposit.js
@@ -1,4 +1,4 @@
-mport { initializeApp, getApps, cert } from "firebase-admin/app";
+import { initializeApp, getApps, cert } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import axios from "axios";
 
